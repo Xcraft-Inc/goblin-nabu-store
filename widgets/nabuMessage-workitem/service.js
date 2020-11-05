ef8e4610e07dedf962c64653fdcfefa31feefc5b
Jonny Quarta <quarta@epsitec.ch>
2020-11-05 11:33:43 +0100

    Increase nabu-message workitem width and add quests

--- widgets/nabuMessage-workitem/service.js
+++ widgets/nabuMessage-workitem/service.js
@@ -1,10 +1,44 @@
 //T:2019-02-27
 const {buildWorkitem} = require('goblin-workshop');
+const {parseParameters} = require('goblin-nabu/lib/format.js');
 
 const config = {
   type: 'nabuMessage',
   kind: 'workitem',
-  quests: {},
+  width: '1400px',
+  initialState: {
+    icuParameters: {},
+    nabuIdIcuError: null,
+  },
+  onLoad: function* (quest) {
+    const message = yield quest.me.getEntityState();
+    const parametersResponse = parseParameters(message.get('nabuId'));
+
+    if (parametersResponse.error) {
+      yield quest.me.change({
+        path: 'nabuIdIcuError',
+        newValue: parametersResponse.error,
+      });
+    } else {
+      const icuParameters = {};
+      for (let parameterName of parametersResponse.parameters) {
+        icuParameters[parameterName] = '';
+      }
+
+      yield quest.me.change({
+        path: 'icuParameters',
+        newValue: icuParameters,
+      });
+    }
+  },
+  quests: {
+    changeIcuParameter: function* (quest, parameterName, value, next) {
+      yield quest.me.change({
+        path: `icuParameters.${parameterName}`,
+        newValue: value,
+      });
+    },
+  },
   hinters: {
     nabuMessage: {
       onValidate: function* (quest, selection) {
